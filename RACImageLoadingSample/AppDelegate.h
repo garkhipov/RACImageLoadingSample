15881a983d14dca8f40094ba471912aba1b2e6a7
Gleb Arkhipov <gleb.arkhipov@gmail.com>
2014-11-26 20:01:33 +0300

    Initial Commit

---
+++ RACImageLoadingSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RACImageLoadingSample
+//
+//  Created by Gleb Arkhipov on 26.11.14.
+//  Copyright (c) 2014 Gleb Arkhipov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RACImageLoadingSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RACImageLoadingSample
+//
+//  Created by Gleb Arkhipov on 26.11.14.
+//  Copyright (c) 2014 Gleb Arkhipov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
